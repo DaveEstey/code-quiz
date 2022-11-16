291ac4cd2e1e9a9a5ca8a5b674831cf290a7ab86
Dave Estey <daveestey.working@gmail.com>
2022-11-16 19:30:04 -0400

    having an issue with each of function and calling index from it

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 
 var question = {
-    qTitle: ["which number is the smallest", "Which number is the largest", "which number is the largest"],
+    qTitle: ["which number is the smallest", "Which number is the largest", "which number is the coolest"],
     options: [[1, 2, 3, 4], [2, 3, 4, 5], [4, 6, 7, 8]],
-    correctanswer: [0, 3, 3],
+    correctanswer: [1, 5, 7],
 
 };
+
 var currQuestion = 0
 var right = 0;
 var wrong = 0;
 var scoreCard = [right, wrong];
-
+var testLength = question.qTitle.length;
 function showQuestion() {
 
     var title = document.getElementById("title");
     title.textContent = question.qTitle[currQuestion];
 
-    var options = document.querySelectorAll(".options");  
-    
+    var options = document.querySelectorAll(".options");
+    console.log(options2);
     options.forEach(function (element, index) {
-        element.textContent = question.options[index];
-        element.addEventListener("click", ".options", function (){
+        console.log(index);
+        element.textContent = question.options[currQuestion][index];
+        element.addEventListener("click", function () {
+            console.log(element.textContent);
+            console.log(question.correctanswer[currQuestion])
+            if (element.textContent == question.correctanswer[currQuestion]) {
+                alert("ding");
+                right++;
+               
+            } else {
+                alert("bong");
+                wrong++;
+             
+            }
+            if (currQuestion < 3){
+                currQuestion++;
+                console.log(currQuestion);
+                showQuestion();
+            }else alert("done!")
             
-            
+           
+        });
     });
-    });
-    
-}
-/* 
-function startQuiz(){
-    var timer= setInterval()
-} */
-
-
-function chooseAnswer(event){
-    event.preventdefault();
-    (this).addEventListener("click", ".choice", this);
-    currQuestion++;
 }
 
 
-showQuestion();
+if (currQuestion == 0){
+    showQuestion();
+}
+
+
+var timer = setInterval(function(){
+
+} , 1000);
