898880a55415c4a3f12879e92e969658bda2e1aa
Dave Estey <daveestey.working@gmail.com>
2022-11-17 02:12:22 -0400

    this is working but I need to check more parameters and then finish timer and scorekeeping and local storage scoreboard

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 var question = {
-    qTitle: ["which number is the smallest", "Which number is the largest", "which number is the coolest"],
-    options: [[1, 2, 3, 4], [2, 3, 4, 5], [4, 6, 7, 8]],
-    correctanswer: [1, 5, 7],
+    qTitle: ["which number is the smallest", "Which number is the largest", "which number is the coolest", "So why does it skip one?", "answer is A"],
+    options: [[1, 2, 3, 4], [2, 3, 4, 5], [4, 6, 7, 8], [7, 9, 9, 10], ["a","b", "c", "d"]],
+    correctanswer: [1, 5, 7, 10, "d"],
     
 };
 var currQuestion = 0;
@@ -12,12 +12,13 @@ var scoreCard = [right, wrong];
 var testLength = question.qTitle.length;
 function showQuestion() {
     
- 
+    console.log("current",currQuestion);
+    
     var title = document.getElementById("title");
     title.textContent = question.qTitle[currQuestion];
 
     var options = document.querySelectorAll(".options");
-    console.log(options);
+    console.log("options",options);
     options.forEach(function (element, index) { 
         console.log("index", index);
         console.log("question", question);
@@ -27,29 +28,30 @@ function showQuestion() {
             console.log(element.textContent);
             console.log(question.correctanswer[currQuestion])
             if (element.textContent == question.correctanswer[currQuestion]) {
-                alert("ding");
+                console.log("right")
                 right++;
                
             } else {
-                alert("bong");
+                console.log("wrong");
                 wrong++;
-             
             }
-            if (currQuestion < 2){
+            if (currQuestion < 4){
+                
                 currQuestion++;
-                console.log(currQuestion);
-                showQuestion();
-            }else alert("done!")
+                console.log("cur", currQuestion);
+                showQuestion(currQuestion);
+                
+            }else {
+                console.log("end of test")
+            }
             
            
         });
     });
 }
 
+showQuestion();
 
-if (currQuestion == 0){
-    showQuestion();
-}
 
 
 var timer = setInterval(function(){
